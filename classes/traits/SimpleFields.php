88f509a9dd54c2c33d87d55fde3b144145672667
Oran Fry <oran@emerg.co>
2024-03-19 21:55:08 +1300

    Internet Addresses
    Add simple field to hold IP addresses - a combined IPv4 and IPv6 field.

--- classes/traits/SimpleFields.php
+++ classes/traits/SimpleFields.php
@@ -244,6 +244,33 @@ trait SimpleFields
         }
     }
 
+    protected function simple_ip_address(string $name, Closure|string|null $default = null): void
+    {
+        $this->fields[$name] = function ($records) use ($name): ?string {
+            if (@$records['/']->$name && $n = inet_pton($records['/']->$name)) {
+                return inet_ntop($n);
+            }
+
+            return null;
+        };
+
+        $this->unfuse_fields[$name] = function ($line) use ($name): ?string {
+            if (@$line->$name && $n = inet_pton($line->$name)) {
+                return inet_ntop($n);
+            }
+
+            return null;
+        };
+
+        $this->completions[] = function ($line) use ($name, $default) {
+            if (!property_exists($line, $name) || $line->$name === null) {
+                $line->$name = static::sf_resolve_value($default, $line);
+            }
+        };
+
+        $this->validations[] = fn ($line) => $line->$name && inet_pton($line->$name) === false ? $name . ' is not a valid ip address' : null;
+    }
+
     protected function simple_latitude(string $name): void
     {
         $this->fields[$name] = fn ($records): ?float => $records['/']->$name;
