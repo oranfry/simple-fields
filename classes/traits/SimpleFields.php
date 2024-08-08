d75151bb637f9cff9dae2f5a1935ed4ed790247a
Oran Fry <oran@emerg.co>
2024-08-08 22:09:49 +1200

    Strings
    Prevent potential warning when unfusing.

--- classes/traits/SimpleFields.php
+++ classes/traits/SimpleFields.php
@@ -330,8 +330,8 @@ trait SimpleFields
 
     protected function simple_string(string $name, Closure|string|null $default = null): void
     {
-        $this->fields[$name] = fn ($records): ?string => $records['/']->$name;
-        $this->unfuse_fields[$name] = fn ($line, $oldline): ?string => @$line->$name;
+        $this->fields[$name] = fn ($records): ?string => $records['/']->$name ?? null;
+        $this->unfuse_fields[$name] = fn ($line, $oldline): ?string => $line->$name ?? null;
 
         $this->completions[] = function ($line) use ($name, $default) {
             if (!property_exists($line, $name) || $line->$name === null) {
