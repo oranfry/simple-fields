74a7293e271a856442065fd46a1c126d46935c64
Oran Fry <oran@emerg.co>
2025-01-17 10:52:16 +1300

    IP Addresses
    Add version-specific field types.

--- classes/traits/SimpleFields.php
+++ classes/traits/SimpleFields.php
@@ -9,6 +9,8 @@ use simplefields\exception\SimpleLiteralDefinitionException;
 
 trait SimpleFields
 {
+    use SimpleIpAddress;
+
     static function sf_resolve_value($value, object $line)
     {
         if ($value instanceof Closure) {
@@ -242,33 +244,6 @@ trait SimpleFields
         foreach (func_get_args() as $name) {
             $this->simple_int($name);
         }
-    }
-
-    protected function simple_ip_address(string $name, Closure|string|null $default = null): void
-    {
-        $this->fields[$name] = function ($records) use ($name): ?string {
-            if (@$records['/']->$name && $n = inet_pton($records['/']->$name)) {
-                return inet_ntop($n);
-            }
-
-            return null;
-        };
-
-        $this->unfuse_fields[$name] = function ($line) use ($name): ?string {
-            if (@$line->$name && $n = inet_pton($line->$name)) {
-                return inet_ntop($n);
-            }
-
-            return null;
-        };
-
-        $this->completions[] = function ($line) use ($name, $default) {
-            if (!property_exists($line, $name) || $line->$name === null) {
-                $line->$name = static::sf_resolve_value($default, $line);
-            }
-        };
-
-        $this->validations[] = fn ($line) => $line->$name && inet_pton($line->$name) === false ? $name . ' is not a valid ip address' : null;
     }
 
     protected function simple_latitude(string $name): void
---
+++ classes/traits/SimpleIpAddress.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace simplefields\traits;
+
+use Closure;
+
+trait SimpleIpAddress
+{
+    protected function simple_ip_address(string $name, Closure|string|null $default = null): void
+    {
+        $this->fields[$name] = function ($records) use ($name): ?string {
+            if (@$records['/']->$name && $n = inet_pton($records['/']->$name)) {
+                return inet_ntop($n);
+            }
+
+            return null;
+        };
+
+        $this->unfuse_fields[$name] = function ($line) use ($name): ?string {
+            if (@$line->$name && $n = inet_pton($line->$name)) {
+                return inet_ntop($n);
+            }
+
+            return null;
+        };
+
+        $this->completions[] = function ($line) use ($name, $default) {
+            if (!property_exists($line, $name) || $line->$name === null) {
+                $line->$name = static::sf_resolve_value($default, $line);
+            }
+        };
+
+        $this->validations[] = fn ($line) => $line->$name && inet_pton($line->$name) === false ? $name . ' is not a valid ip address' : null;
+    }
+
+    protected function simple_ipv4_address(string $name, Closure|string|null $default = null): void
+    {
+        $this->simple_ip_address($name, $default);
+
+        $this->validations[] = fn ($line) => $line->$name && strpos($line->$name, '.') === false ? $name . ' is not an ipv4 address' : null;
+    }
+
+    protected function simple_ipv6_address(string $name, Closure|string|null $default = null): void
+    {
+        $this->simple_ip_address($name, $default);
+
+        $this->validations[] = fn ($line) => $line->$name && strpos($line->$name, ':') === false ? $name . ' is not an ipv6 address' : null;
+    }
+}
